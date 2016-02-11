927573ee8bce2654526b78b9dc3b3635d87cda2e
Ben Slater <ben@overmation.com>
2016-02-11 14:29:00 +0000

    added test for navigating between detail and listing pages. added support directory and attributes file for containing common methods

--- spec/features/show_event_spec.rb
+++ spec/features/show_event_spec.rb
@@ -3,11 +3,7 @@ require "spec_helper"
 describe "Viewing an individual event" do
 	it "shows the events details" do
 
-		event = Event.create(name: "BugSmash",
-							  location: "Denver",
-							  price: "10.00",
-							  description: "A fun evening of bug smashing",
-							  starts_at: "10.days.from_now")
+		event = Event.create(event_attributes(price: 10.00))
 
 		visit event_url(event)
 
