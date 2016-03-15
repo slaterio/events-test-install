e445bcf8aecbce01286254aae13b20e9333ca39c
Ben Slater <ben@overmation.com>
2016-03-15 12:03:28 +0000

    added form partial to views. added autofocus to name field. added cancel button to form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+root "events#index"
 resources :events
-#  root "events#index"
 #  get "events" => "events#index"
 #  get "events/:id" => "events#show", as: "event"
 #  get "events/:id/edit" => "events#edit", as: "edit_event"
