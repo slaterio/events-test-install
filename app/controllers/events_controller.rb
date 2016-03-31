973612a5c4c7596817cb780ced906dd00da9c74f
Ben Slater <ben@overmation.com>
2016-03-31 16:09:31 +0100

    added flash messages to form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
 	def update
 		@event = Event.find(params[:id])
 		if @event.update(event_params)
-			redirect_to @event
+			redirect_to @event, notice: "Event successfully updated!"
 		else 
 			render :edit
 		end
