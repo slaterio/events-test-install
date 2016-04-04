a0d5e6d0ab2d27b4756deecd50b0a6f7f28c2183
Ben Slater <ben@overmation.com>
2016-04-04 17:16:40 +0100

    nested registrations resources and created individual lists or registrations for each event

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :registrations
+  
 root "events#index"
-resources :events
+resources :events do 
+	resources :registrations
+end
 #  get "events" => "events#index"
 #  get "events/:id" => "events#show", as: "event"
 #  get "events/:id/edit" => "events#edit", as: "edit_event"
