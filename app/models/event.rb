a33aa8fdc28a452de52e2e3f21753acd0d6cd390
Ben Slater <ben@overmation.com>
2016-04-05 17:18:23 +0100

    added business logic to registrations to show remaining spots and sold out button

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,4 +23,12 @@ has_many :registrations, dependent: :destroy
 		where("starts_at >= ?", Time.now).order("starts_at")
 	end
 
+	def spots_left
+		capacity - registrations.size
+	end
+
+	def sold_out?
+		spots_left.zero?
+	end
+
 end
